33405d57f3d50d33eeb1049d9c441a6ec4a93c5f
kennywonda <ib.kehinde00@gmail.com>
2022-06-29 15:40:02 -0700

    adding up project

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "registeration";
+
+    // to connect
+    $conn = mysqli_connect($servername, $username, $password, $database) or die("Database connection failed");
+    
+    ?>
---
+++ welcome.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+// code so that it wont go back after logging out 
+if(!isset($_SESSION["user_id"])){
+    header("Location: index.php");
+}
+
+echo $_SESSION["user_id"];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>logout</title>
+</head>
+<body>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
+
